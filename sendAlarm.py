f39cbaa4dbb12e723be7bab4f8eeb2897983f00d
sjoon2455 <sjoon2455@kaist.ac.kr>
2019-07-25 16:47:46 +0900

    working on typeIdPwd:

--- sendAlarm.py
+++ sendAlarm.py
@@ -1,14 +1,17 @@
 #-*- coding:utf-8 -*-
 import subprocess
 import psutil
+from typeIdPwd import typeIdPwd
 ### during some job... if loginpage occured, this function need to be called
 
 def suspendAlarmResume():
-    #get current pid
-    currentpid = psutil.getPid(some process)
+    currentpid = psutil.getPid(some process) #get current pid
     p = psutil.Process(currentpid)
     
     p.suspend()
     subprocess.call("say 'beep'", shell = True) #beep
     ### get id, pwd from user and type them in. 
+    user_id = input("ID: ")
+    user_pw = input("PASSWORD: \n(If none, press return button with empty input)")
+    typeIdPwd(user_id, user_pw)
     p.resume()
--- typeIdPwd.py
+++ typeIdPwd.py
@@ -1,1 +1,14 @@
 #-*- coding:utf-8 -*-
+import subprocess
+
+### input: id(str), password(str), page description(xml)
+# input text id -> input tap -> (조건문) -> input text pwd
+def typeIdPwd(id, pwd, xml):
+    cmd = "adb shell input text '{0}'".format(id)
+    proc = subprocess.Popen(
+        cmd,
+        shell = True,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE
+    )
+    out, err = proc.communicate()
