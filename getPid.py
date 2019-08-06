743b7c83dedacf3a2ad4e4b112c7ef19de3a3783
sjoon2455 <sjoon2455@kaist.ac.kr>
2019-08-06 16:22:01 +0900

    get pid done

--- getPid.py
+++ getPid.py
@@ -32,13 +32,13 @@ def androidGetPid(process):
         stderr = subprocess.PIPE
     )
     out, err = proc.communicate()
+    out = b'shell         6468  1987 1188536  72988 binder_thread_read abe8eac4 S com.android.commands.monkey\n'
+    #print(type(out), out)
     decode_out = out.decode('utf-8')
     #print(type(decode_out), decode_out)
-    decode_out_split = decode_out.split("\n")
-    ps = decode_out_split[0]
-    #print(type(ps), ps)
-    index = getIndex(ps)
-    res = ps[:index]
+    decode_out_split = decode_out.split(" ")
+    #print(decode_out_split[9])
+    res = decode_out_split[9]
     return int(res)
 
 ### input: string
@@ -54,5 +54,5 @@ def getIndex(ps):
             return count
             
 #print(getIndex("1 dsd")) # should print 1
-print(pcGetPid("qemu"))
-#androidGetPid("monkey")
+#print(pcGetPid("qemu"))
+androidGetPid("monkey")
